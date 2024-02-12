05e3b6c030359c7af729b5689f06c1ce0ee038e5
Bzjshehd <159775093+Bzjshehd@users.noreply.github.com>
2024-02-12 22:30:43 +0800

    Add files via upload

---
+++ warnFunction.js
@@ -0,0 +1,15 @@
+
+let btnOo = document.getElementById("btnOo");
+let btnDire = document.getElementById("btnDire");
+
+btnOo.onclick = function(){
+document.getElementById("enter").play();
+    window.location.assign("page2.html");
+    
+}
+
+btnDire.onclick = function(){
+document.getElementById("enter").play();
+    window.location.assign("index.html");
+    
+}
